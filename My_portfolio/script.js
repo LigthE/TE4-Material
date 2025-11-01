6cd6bf9d2503596cebd05592a8701e9fc91eca1c
Erkan.Akdag <erkan.akdag@erkanakdag-GWR45R7GK2.local>
2025-11-01 22:49:42 +0100

    initial commit

--- My_portfolio/script.js
+++ My_portfolio/script.js
@@ -1,1 +1,38 @@
 console.log("Hello Wanderer");
+
+let darkmode = localStorage.getItem(`darkmode`);
+const themeSwitch = document.getElementById(`theme-switch`);
+
+const enableDarkmode = () => {
+  document.body.classList.add(`darkmode`);
+  localStorage.setItem(`darkmode`, `active`);
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove(`darkmode`);
+  localStorage.setItem(`darkmode`, null);
+};
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
+
+const BackToTop = document.getElementById("BackToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    BackToTop.classList.add("show");
+  } else {
+    BackToTop.classList.remove("show");
+  }
+});
+
+BackToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  BackToTop.classList.remove("show");
+});
