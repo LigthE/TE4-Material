e2a66940454d02f6e4be7eea64e1e43df7df6b08
Erkan.Akdag <erkan.akdag@erkanakdag-GWR45R7GK2.local>
2025-11-05 19:11:10 +0100

    initial commit jag har fixat nu scroll to top , ligth mode dark , och sticky menu och scroll animations

--- My_portfolio/script.js
+++ My_portfolio/script.js
@@ -1,5 +1,15 @@
 console.log("Hello Wanderer");
 
+/* låt darkmode lägga darkmode text i localstorage
+konstant themeswitch 
+kör konstant eneabledarkmode lägg till darkmode i local storage
+om aktiv
+
+const disabledarkmode den tar bort darkmode från localstorage
+och lägger null vilket står för none
+osen themeswitch.acceventlistener när klikas om darkmode inte aktive 
+då aktivera darkmode annars ta bort darkmode*/
+
 let darkmode = localStorage.getItem(`darkmode`);
 const themeSwitch = document.getElementById(`theme-switch`);
 
@@ -14,6 +24,14 @@ const disableDarkmode = () => {
 };
 
 themeSwitch.addEventListener("click", () => {
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+/*när det klikas på darkmode knappen om darkmode är inte aktive  då aktiveras det darkmode
+annars det deaktiveras och darkmode getitem visar darkmode som text inne i localstorage
+för att stora info där*/
+
+themeSwitch.addEventListener("click", () => {
   darkmode = localStorage.getItem("darkmode");
   if (darkmode !== "active") {
     enableDarkmode();
@@ -21,6 +39,10 @@ themeSwitch.addEventListener("click", () => {
     disableDarkmode();
   }
 });
+
+/*när scrollas mer än 200px back to top button visas annars t.ex inte kommit till 200px den visas inte
+men add hämtar knappen gör den synlig och remove visar den inte den visar inte om du har tryckt på 
+knappen eller om du inte har scrollat 200px neråt*/
 
 const BackToTop = document.getElementById("BackToTop");
 
@@ -35,4 +57,20 @@ window.addEventListener("scroll", () => {
 BackToTop.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
   BackToTop.classList.remove("show");
+});
+
+/* när scrollas så körs det reveal och om det scrollas ner 
+så körs active reveal*/
+
+window.addEventListener("scroll", () => {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
 });
